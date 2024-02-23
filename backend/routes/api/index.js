1074a709c274a624320af1daeca4a79f893033c1
Brian Carmichael <bdonjr7@gmail.com>
2024-02-23 14:37:27 -0800

    Add User input validation on user signup backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,6 +7,9 @@ const { restoreUser } = require("../../utils/auth.js");
 router.get("/restore-user", (req, res) => {
   return res.json(req.user);
 });
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
