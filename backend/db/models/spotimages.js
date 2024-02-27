2043b0e1d33be57ba146df134d4632a4dc0e89ac
Brian Carmichael <bdonjr7@gmail.com>
2024-02-26 22:14:05 -0800

    database v1 final

---
+++ backend/db/models/spotimages.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class SpotImages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      SpotImages.belongsTo(models.Spot, {
+        foreignKey: spotId,
+      });
+    }
+  }
+  SpotImages.init(
+    {
+      spotId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING,
+      },
+      preview: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: "SpotImages",
+    }
+  );
+  return SpotImages;
+};
