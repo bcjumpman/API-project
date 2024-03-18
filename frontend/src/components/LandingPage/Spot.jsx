a8ca6c8e1c5b61ccc730228667488e24a383405d
Brian Carmichael <bdonjr7@gmail.com>
2024-03-18 14:25:34 -0700

    landing page and navbar update

---
+++ frontend/src/components/LandingPage/Spot.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Spot({ spot }) {
+  return (
+    <div className="spot-card" title={spot.name}>
+      <Link to={`/spots/${spot.id}`}>
+        <img
+          src={spot.previewImage}
+          alt={spot.name}
+          className="spot-card-image"
+          key={spot.id}
+        />
+        <div className="spot-info">
+          <p>
+            {spot.city}, {spot.state}
+          </p>
+          <p>
+            <i className="fa-solid fa-star" />
+            {(spot.avgRating && Number(spot.avgRating).toFixed(1)) || `New`}
+          </p>
+        </div>
+        <p>${spot.price} night</p>
+      </Link>
+    </div>
+  );
+}
+
+export default Spot;
