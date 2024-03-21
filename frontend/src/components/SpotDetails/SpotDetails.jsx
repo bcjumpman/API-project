a4272d505101f5f667542855d332cd9b8470ca3d
Brian Carmichael <bdonjr7@gmail.com>
2024-03-20 17:04:51 -0700

    reviews started

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -2,15 +2,21 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchSpot } from "../../store/spots";
+import { fetchReviews } from "../../store/reviews";
 
 function SpotDetails() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spot = useSelector((state) => state.spots[spotId]);
-  // const images = spot? .SpotImages;
+
+  const reserveBtn = (e) => {
+    e.preventDefault();
+    alert("Feature coming soon");
+  };
 
   useEffect(() => {
     dispatch(fetchSpot(spotId));
+    dispatch(fetchReviews(spotId));
   }, [dispatch, spotId]);
 
   return (
@@ -29,6 +35,52 @@ function SpotDetails() {
             spot.SpotImages.map((image, index) => (
               <img key={index} src={image.url} />
             ))}
+        </div>
+      </div>
+      <div className="spot-details">
+        <div className="spot-description">
+          <h3>
+            Hosted by {spot?.Owner?.firstName} {spot?.Owner?.lastName}
+          </h3>
+          <p>{spot?.description}</p>
+        </div>
+        <div className="reserve-container">
+          <div className="reserve-top">
+            <p>${spot?.price} night</p>
+            {spot?.numReviews === 0 ? (
+              <div className="reserve-top-right">
+                <i className="fa-solid fa-star" />
+                <span>New</span>
+              </div>
+            ) : (
+              <div className="reserve-top-right">
+                <i className="fa-solid fa-star" />
+                {Number(spot?.avgStarRating).toFixed(1)}
+                <span> · </span>
+                {spot?.numReviews} {spot?.numReviews > 1 ? "reviews" : "review"}
+              </div>
+            )}
+          </div>
+          <button className="reserve-btn" onClick={reserveBtn}>
+            Reserve
+          </button>
+        </div>
+        <div className="reviews-header">
+          <div>
+            {spot?.numReviews === 0 ? (
+              <div>
+                <i className="fa-solid fa-star" />
+                <span>New</span>
+              </div>
+            ) : (
+              <div>
+                <i className="fa-solid fa-star" />
+                {Number(spot?.avgStarRating).toFixed(1)}
+                <span> · </span>
+                {spot?.numReviews} {spot?.numReviews > 1 ? "reviews" : "review"}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
